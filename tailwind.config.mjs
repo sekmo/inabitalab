79a766189fe5b9010d6e1f6274fa925a0459d35d
Francesco Mari <francescojjmari@gmail.com>
2024-01-01 20:23:11 +0100

    Have explicit font sizes and line heights for every tailwind font size class

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    fontSize: {
+      // this are the tailwind defaults sizes and line heights.
+      // are listed here so it's easy to override to experiment with them.
+      xs: ['0.75rem', '1rem'],
+      sm: ['0.875rem', '1.25rem'],
+      base: ['1rem', '1.5rem'],
+      lg: ['1.125rem', '1.75rem'],
+      xl: ['1.25rem', '1.75rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['1.875rem', '2.25rem'],
+      '4xl': ['2.25rem', '2.5rem'],
+      '5xl': ['3rem', '1'],
+      '6xl': ['3.75rem', '1'],
+      '7xl': ['4.5rem', '1'],
+      '8xl': ['6rem', '1'],
+      '9xl': ['8rem', '1'],
+    },
+  },
+  plugins: [],
 }
