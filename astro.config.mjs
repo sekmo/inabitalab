f7bbb2bb784a2711c57fce52a4b6da28df1119f6
Francesco Mari <francescojjmari@gmail.com>
2026-08-18 18:11:14 +0200

    Upgrade to Astro 7, Vercel adapter 11, Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
